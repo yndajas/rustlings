5026ca3cf9661b1d25f99224a15ca9296a3c07ba
Ynda Jas <yndajas@gmail.com>
2024-12-01 04:45:15 +0000

    Solve if 2
    I originally wrote this with an `if / else if / else` statement:
    
    ```rs
    if food == "strawberry" {
        "Yummy!"
    } else if food == "potato" {
        "I guess I can eat that."
    } else {
        "No thanks!"
    }
    ```
    
    ... but the `match` statement feels more Rust-y

--- exercises/03_if/if2.rs
+++ exercises/03_if/if2.rs
@@ -1,9 +1,9 @@
 // TODO: Fix the compiler error on this function.
 fn picky_eater(food: &str) -> &str {
-    if food == "strawberry" {
-        "Yummy!"
-    } else {
-        1
+    match food {
+        "strawberry" => "Yummy!",
+        "potato" => "I guess I can eat that.",
+        _ => "No thanks!",
     }
 }
 
